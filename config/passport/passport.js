3ab67bb8412c634b24729f8c9a831227cef77869
Lewis Fong <fongl@bu.edu>
2018-04-13 14:23:03 -0700

    models wip

---
+++ config/passport/passport.js
@@ -0,0 +1,124 @@
+var bCrypt = require("bcrypt-nodejs");
+
+module.exports = function(passport, user) {
+  var User = user;
+
+  var LocalStrategy = require("passport-local").Strategy;
+
+  //serialize
+  passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+
+  // deserialize user
+  passport.deserializeUser(function(id, done) {
+    User.findById(id).then(function(user) {
+      if (user) {
+        done(null, user.get());
+      } else {
+        done(user.errors, null);
+      }
+    });
+  });
+  passport.use(
+    "local-signup",
+    new LocalStrategy(
+      {
+        usernameField: "username",
+        passwordField: "password",
+        passReqToCallback: true // allows us to pass back the entire request to the callback
+      },
+      function(req, hotdog, password, done) {
+        var generateHash = function(password) {
+          return bCrypt.hashSync(password, bCrypt.genSaltSync(8), null);
+        };
+        User.findOne({
+          where: {
+            username: hotdog
+          }
+        }).then(function(user) {
+          if (user) {
+            return done(null, false, {
+              message: "That username is already taken"
+            });
+          } else {
+            var userPassword = generateHash(password);
+
+            var data = {
+              username: hotdog,
+
+              password: userPassword,
+
+              firstname: req.body.firstname,
+
+              lastname: req.body.lastname
+            };
+
+            User.create(data).then(function(newUser, created) {
+              if (!newUser) {
+                return done(null, false);
+              }
+
+              if (newUser) {
+                return done(null, newUser);
+              }
+            });
+          }
+        });
+      }
+    )
+  );
+
+  //LOCAL SIGNIN
+  passport.use(
+    "local-signin",
+    new LocalStrategy(
+      {
+        // by default, local strategy uses username and password, we will override with email
+
+        usernameField: "username",
+
+        passwordField: "password",
+
+        passReqToCallback: true // allows us to pass back the entire request to the callback
+      },
+
+      function(req, hotdog, password, done) {
+        var User = user;
+
+        var isValidPassword = function(userpass, password) {
+          return bCrypt.compareSync(password, userpass);
+        };
+
+        User.findOne({
+          where: {
+            username: hotdog
+          }
+        })
+          .then(function(user) {
+            if (!user) {
+              return done(null, false, {
+                message: "user does not exist"
+              });
+            }
+
+            if (!isValidPassword(user.password, password)) {
+              return done(null, false, {
+                message: "Incorrect password."
+              });
+            }
+
+            var userinfo = user.get();
+            return done(null, userinfo);
+          })
+          .catch(function(err) {
+            console.log("Error:", err);
+
+            return done(null, false, {
+              message: "Something went wrong with your Signin"
+            });
+          });
+      }
+    )
+  );
+};
