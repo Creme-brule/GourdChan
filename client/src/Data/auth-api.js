60a956dd8c558f953f65a054675f881923d8604c
Lewis Fong <fongl@bu.edu>
2018-04-19 11:01:25 -0700

    wip

--- client/src/Data/auth-api.js
+++ client/src/Data/auth-api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const authApi = {
-  create: org => axios.post('/auth', org).then(results => results.data),
+  create: org => axios.post('/auth/signup', org).then(results => results.data),
   update: org => axios.put(`/auth/${org.id}`, org),
 };
 export {
