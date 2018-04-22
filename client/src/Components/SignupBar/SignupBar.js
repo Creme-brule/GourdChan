2931fedf0485d0e9cc59081bcd8bed9d7a77160a
Justin <justinho97@gmail.com>
2018-04-22 13:39:52 -0700

    login/singup

--- client/src/Components/SignupBar/SignupBar.js
+++ client/src/Components/SignupBar/SignupBar.js
@@ -34,7 +34,7 @@ class SignupBar extends React.Component {
     render() {
         return (
             <div className="userBox">
-               
+                    <p>Sign Up</p>
                     <input
                         value={this.state.username}
                         name="username"
@@ -52,6 +52,7 @@ class SignupBar extends React.Component {
                         placeholder="Password"
                     />
                     <button onClick={this.createFormSubmit}>Submit</button>
+                    <button onClick={this.props.swap}>SIGN IN/UP</button>
                     {/* <button onClick={this.props.signup(this.state.username,this.state.password)}>Submit</button> */}
                 
             </div>
