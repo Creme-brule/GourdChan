a59a7f36f81b7ecd4b93ac2296c396dc9068dae1
Lewis Fong <fongl@bu.edu>
2018-04-25 19:33:05 -0700

    jawsdb new

--- config/database.js
+++ config/database.js
@@ -1,9 +1,9 @@
 require('./env');
 module.exports = {
   //depending on where deployed, these could change.
-  "username": "root",
-  "password": "gothic2",
-  "database": "gourdchan_db",
-  "host": "127.0.0.1",
+  "username": "fk6uc0ej3p9vtdah",
+  "password": "h1j0s6nv9xu844jf",
+  "database": "viytptg6e8ljng7b",
+  "host": "umabrisfx8afs3ja.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   "dialect": "mysql"
 }
