ab762cfb2aff1fda7c17361bced2d449d63a0ab1
Lewis Fong <fongl@bu.edu>
2018-04-25 19:28:40 -0700

    unknown error fix

--- config/database.js
+++ config/database.js
@@ -1,9 +1,9 @@
 require('./env');
 module.exports = {
   //depending on where deployed, these could change.
-  "username": "f2q62ufso6mhn4pv",
-  "password": "d7r1y71mc9dx3ipp",
-  "database": "bb2u4mk3t5jscxkx",
-  "host": "op2hpcwcbxb1t4z9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  "username": "root",
+  "password": "gothic2",
+  "database": "gourdchan_db",
+  "host": "127.0.0.1",
   "dialect": "mysql"
 }
