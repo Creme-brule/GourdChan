d30c95bd8323a071f76b1229ad2ee82a74b26939
Lewis Fong <fongl@bu.edu>
2018-04-25 19:35:12 -0700

    force:true remove

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ import db from './server/models';
 const app = App(__dirname);
 
 //use sync({force:true}) to drop all tables before trying to create
-db.sequelize.sync({force:true}).then(function() {
+db.sequelize.sync({}).then(function() {
   app.listen(PORT, function() {
     console.log('App listening on PORT: ' + PORT);
   });
